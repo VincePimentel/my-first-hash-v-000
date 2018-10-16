e4c4d9d1150c189760b25f0177d202f11ac6eb2f
Vince Andrew Pimentel <contact@vincep.me>
2018-10-16 23:42:14 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  favorite_games_by_genre = {"MMO" => "Black Desert Online", "FPS" => "Counter-Strike: Global Offensive", "RTS" => "Starcraft II"}  
 end
 
 
